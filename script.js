3975d9e6a6171cfbaa0a3c12455d13f88997553b
Subhan Kausar <126421273+Subhankausar@users.noreply.github.com>
2023-10-04 16:31:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-describe("Test First Non-Repeated Character", () => {
-  it("should handle a string with the first non-repeated character", () => {
-    const baseUrl = "http://your-app-url"; // Replace with your application's URL
+function firstNonRepeatedChar(str) {
+  // Create an object to store character frequencies
+  const charCount = {};
 
-    cy.visit(baseUrl, {
-      onBeforeLoad(win) {
-        // Stub the prompt function to return 'c' (first non-repeated character)
-        cy.stub(win, "prompt").onFirstCall().returns('c');
-      }
-    });
+  // Iterate through the string and count the frequency of each character
+  for (const char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
 
-    cy.on("window:alert", result => {
-      expect(result).to.equal('c'); // Expect the alert result to be 'c'
-    });
-  });
+  // Iterate through the string again to find the first non-repeated character
+  for (const char of str) {
+    if (charCount[char] === 1) {
+      return char; // Found the first non-repeated character
+    }
+  }
 
-  it("should handle a string with no non-repeated character", () => {
-    const baseUrl = "http://your-app-url"; // Replace with your application's URL
+  // If no non-repeated character is found, return null
+  return null;
+}
 
-    cy.visit(baseUrl, {
-      onBeforeLoad(win) {
-        // Stub the prompt function to return null (no non-repeated character)
-        cy.stub(win, "prompt").onFirstCall().returns(null);
-      }
-    });
-
-    cy.on("window:alert", result => {
-      expect(result).to.equal(null); // Expect the alert result to be null
-    });
-  });
-});
+// Test cases
+console.log(firstNonRepeatedChar('aabbcdd')); // Output: 'c'
+console.log(firstNonRepeatedChar('aabbcc'));  // Output: null
